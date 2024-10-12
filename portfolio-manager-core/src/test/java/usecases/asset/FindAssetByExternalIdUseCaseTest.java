9c04fe947c08e02d97217f0544083512daedc6da
FPetronilho <jfgp_10@live.com.pt>
2024-10-12 08:28:49 +0200

    Implemented find asset by external ID use case unit test.

--- portfolio-manager-core/src/test/java/usecases/asset/FindAssetByExternalIdUseCaseTest.java
+++ portfolio-manager-core/src/test/java/usecases/asset/FindAssetByExternalIdUseCaseTest.java
@@ -1,23 +1,51 @@
 package usecases.asset;
 
 import com.portfolio.portfoliomanager.dataprovider.PortfolioManagerDataProvider;
+import com.portfolio.portfoliomanager.domain.Asset;
+import com.portfolio.portfoliomanager.usecases.asset.FindAssetByExternalIdUseCase;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 public class FindAssetByExternalIdUseCaseTest {
 
     @Mock
-    private PortfolioManagerDataProvider portfolioManagerDataProvider;
+    private PortfolioManagerDataProvider dataProvider;
 
     @InjectMocks
-    private FindAssetByExternalIdUseCaseTest findAssetByExternalIdUseCaseTest;
+    private FindAssetByExternalIdUseCase findAssetByExternalIdUseCase;
 
     @BeforeEach
     void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
 
+    @Test
+    void testExecute_shouldReturnAsset() {
+        // Given
+        String digitalUserId = "test-digitalUserId";
+        String externalId = "test-externalId";
+
+        Asset expectedAsset = new Asset();
+
+        FindAssetByExternalIdUseCase.Input input = FindAssetByExternalIdUseCase.Input.builder()
+                .digitalUserId(digitalUserId)
+                .externalId(externalId)
+                .build();
+
+        when(dataProvider.findAssetByExternalId(digitalUserId,externalId)).thenReturn(expectedAsset);
+
+        // When
+        FindAssetByExternalIdUseCase.Output result = findAssetByExternalIdUseCase.execute(input);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(expectedAsset, result.getAsset());
     }
 }
